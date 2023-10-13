435caf4990cff1cbf694503b20e4693cf9390867
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-10-13 07:26:31 -0400

    PromoSection ready

---
+++ src/components/PromoSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './PromoSection.css'
+import imageGearDesktop from '../assets/shared/desktop/image-best-gear.jpg'
+
+export default function PromoSection () {
+  return (
+    <section className='promo-section'>
+      <section className='text-section'>
+        <h2>BRINGING YOU THE <span>BEST</span> AUDIO GEAR</h2>
+        <p>Located at the heart of New York City, Audiophile is the prmier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstrarion rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+      </section>
+      <img src={imageGearDesktop} alt='Promo image' />
+    </section>
+  )
+}
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import PromoSection from '../components/PromoSection'
 
 export default function App () {
   return (
@@ -9,10 +10,13 @@ export default function App () {
       <header>
         <Header />
       </header>
-      <Routes>
-        <Route path='/' element={<p>Root</p>} />
-        <Route path='/other' element={<p>other</p>} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path='/' element={<p>Root</p>} />
+          <Route path='/other' element={<p>other</p>} />
+        </Routes>
+        <PromoSection />
+      </main>
       <Footer />
     </>
   )
