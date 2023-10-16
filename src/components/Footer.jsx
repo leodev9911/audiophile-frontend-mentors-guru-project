b619416d45cfc2464456a2937369f7513f0c99c3
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-10-16 11:45:50 -0400

    Styles fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,47 +8,49 @@ import './Footer.css'
 export default function Footer () {
   return (
     <footer>
-      <section className='nav-footer'>
-        <img
-          src={logo} alt='Audiophile logo'
-          className='logo'
-        />
-        <section className='right-section'>
-          <ul className='unordered-list__nav'>
-            <li>
-              <a href='#'>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                Headphones
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                Speakers
-              </a>
-            </li>
-            <li>
-              <a href='#'>
-                Earphones
-              </a>
-            </li>
-          </ul>
+      <section className='footer-section'>
+        <section className='nav-footer'>
+          <img
+            src={logo} alt='Audiophile logo'
+            className='logo'
+          />
+          <section className='right-section'>
+            <ul className='unordered-list__nav'>
+              <li>
+                <a href='#'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  Headphones
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  Speakers
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  Earphones
+                </a>
+              </li>
+            </ul>
+          </section>
         </section>
+        <p className='footer-text__container'>
+          Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.
+        </p>
+        <section className='links-section'>
+          <a><img src={facebook} alt='Facebook link' /></a>
+          <a><img src={twitter} alt='Twitter link' /></a>
+          <a><img src={instagram} alt='Instagram link' /></a>
+        </section>
+        <p className='copyright'>
+          Copyright © 2022. All Rights Reserved
+        </p>
       </section>
-      <p className='footer-text__container'>
-        Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.
-      </p>
-      <section className='links-section'>
-        <a><img src={facebook} alt='Facebook link' /></a>
-        <a><img src={twitter} alt='Twitter link' /></a>
-        <a><img src={instagram} alt='Instagram link' /></a>
-      </section>
-      <p className='copyright'>
-        Copyright © 2022. All Rights Reserved
-      </p>
     </footer>
   )
 }
