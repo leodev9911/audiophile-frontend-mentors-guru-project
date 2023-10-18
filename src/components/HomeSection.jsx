e4f36e9931abf982e81c86952fbabd2ea5daac8a
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-10-17 23:39:34 -0400

    Earphone home ready

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './HomeSection.css'
 import speakerZX9 from '../assets/product-zx9-speaker/desktop/image-speaker-zx9.14ec07a6.png'
+import earphone from '../assets/home/desktop/image-earphones-yx1.jpg'
 
 export default function HomeSection () {
   return (
@@ -18,6 +19,17 @@ export default function HomeSection () {
       </div>
       <div className='speaker-zx7-container'>
         <div className='speaker-zx7-text__cotainer'>
+          <h2>ZX7 SPEAKER</h2>
+          <a href='' className='first-section-link'>
+            SEE PRODUCT
+          </a>
+        </div>
+      </div>
+      <div className='earphones-container'>
+        <div className='image-container'>
+          <img src={earphone} alt='Earphone' />
+        </div>
+        <div className='earphones-text__cotainer'>
           <h2>ZX7 SPEAKER</h2>
           <a href='' className='first-section-link'>
             SEE PRODUCT
